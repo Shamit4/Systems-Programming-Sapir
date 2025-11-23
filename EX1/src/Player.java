cc2bfa9e7ae32241bbf7229b513af9542089efb6
Shamit4 <shamit415@gmail.com>
2025-11-23 19:03:41 +0200

    Refactor Player class for improved readability

--- EX1/src/Player.java
+++ EX1/src/Player.java
@@ -1,1 +1,43 @@
-public class Player extends Character { private Item[] inventory; public Player(Item[] inv){super(); this.inventory=inv;} public void addItem(Item i){ for(int x=0;x<inventory.length;x++){ if(inventory[x]==null){inventory[x]=i; return;}}} public int calculateInventoryValue(){int s=0; for(Item it:inventory){ if(it!=null)s+=it.getValue();} return s;} public boolean hasMagicalItem(){ for(Item it:inventory){ if(it!=null && it.isMagical()) return true;} return false;} public String toString(){ String b=super.toString(); String inv="Inventory: "; for(Item it:inventory){ if(it!=null) inv+=it.getName()+" | "; } return b+"\n"+inv;} }
+// 204328611_207845611
+
+public class Player extends Character
+  { 
+    private Item[] inventory; 
+    public Player(Item[] inv)
+    {
+      super(); 
+      this.inventory=inv;
+    } 
+    public void addItem(Item i)
+    { 
+      for(int x=0;x<inventory.length;x++)
+        {
+          if(inventory[x]==null) 
+            inventory[x]=i;
+          return;       
+        }
+    }
+    public int calculateInventoryValue()
+    {
+      int s=0;
+      for (Item it:inventory)
+        if(it!=null) s+=it.getValue();
+      return s;
+    } 
+    public boolean hasMagicalItem()
+    { 
+      for(Item it:inventory)       
+          if(it!=null && it.isMagical()) return true;
+      return false;
+    }
+    public String toString()
+    { 
+      String b=super.toString();
+      String inv="Inventory: ";
+      for (Item it:inventory)
+      {
+        if(it!=null) inv+=it.getName()+" | "; 
+      } 
+      return b+"\n"+inv;
+    } 
+  }
